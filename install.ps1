c3798613eb242d41b1b54752d19b7fa135fa50ac
Oleksii <76210733+KernelKrise@users.noreply.github.com>
2026-05-22 19:02:41 +0300

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,6 +1,7 @@
-# Variables
+# Constants
 $httpyUrl = "https://raw.githubusercontent.com/KernelKrise/httpy/refs/heads/main/httpy"
 $httpyBatUrl = "https://raw.githubusercontent.com/KernelKrise/httpy/refs/heads/main/httpy.bat"
+$contextMenuLabel = "Launch HTTPY here"
 
 # Logging
 function Write-ILog {
@@ -37,5 +38,16 @@ if (-not ($pathFolders -contains $scriptsPath)) {
 Write-Ilog "Downloading httpy"
 Invoke-WebRequest -Uri "${httpyUrl}" -OutFile "${scriptsPath}/httpy.py"
 Invoke-WebRequest -Uri "${httpyBatUrl}" -OutFile "${scriptsPath}/httpy.bat"
+
+# Contex menu on desktop / folder
+$contextMenuCommand = "cmd.exe /c '${scriptsPath}/httpy.bat'"
+$pathBg = "HKCR:\Directory\Background\shell\httpy"
+if (-not (Get-PSDrive -Name HKCR -ErrorAction SilentlyContinue)) {
+    New-PSDrive -Name HKCR -PSProvider Registry -Root HKEY_CLASSES_ROOT | Out-Null
+}
+if (-not (Test-Path "${pathBg}")) { New-Item -Path "${pathBg}" -Force | Out-Null }
+Set-ItemProperty -Path "${pathBg}" -Name "(Default)" -Value "$contextMenuLabel"
+if (-not (Test-Path "${pathBg}\command")) { New-Item -Path "${pathBg}\command" -Force | Out-Null }
+Set-ItemProperty -Path "${pathBg}\command" -Name "(Default)" -Value "${contextMenuCommand}"
 
 Write-Ilog "httpy installed"
